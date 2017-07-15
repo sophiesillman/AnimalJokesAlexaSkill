0375c6607017233019e5f24db52098aaeeb90cdf
sophiesillman <sophiesillman@gmail.com>
2017-07-15 21:59:56 +0100

    Refactor class a bit

---
+++ AnimalJokes/Data/JokeResource.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace AnimalJokes.Data
+{
+    public class JokeResource
+    {
+        public JokeResource(string language)
+        {
+            Language = language;
+            Jokes = new List<string>();
+        }
+
+        public string Language { get; set; }
+        public string SkillName { get; set; }
+        public List<string> Jokes { get; set; }
+        public string GetJokeMessage { get; set; }
+        public string HelpMessage { get; set; }
+        public string HelpReprompt { get; set; }
+        public string StopMessage { get; set; }
+    }
+}
